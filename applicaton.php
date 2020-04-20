599b2a472b92b53e46ad7a54af91dc73eaaee19f
Dulal-Ahammed <63893957+Dulal-Ahammed@users.noreply.github.com>
2020-04-20 17:57:17 +1400

    Add files via upload

---
+++ applicaton.php
@@ -0,0 +1,31 @@
+<html>
+<head></head>
+<body>
+<form>
+Name:<input type="text" name="name"><br><br>
+Email:<input type="emaill" name="email"><br><br>
+Pssword:<input type="password" name="password"><br><br>
+City:<select>
+<option>---select---</option>
+<option>Gurgoan</option>
+<option>Noida</option>
+<option>Delhi</option>
+<option>loknu</option>
+<option>Kolkata</option>
+<option>Malda</option>
+</select><br><br>
+Gender<br><br>
+<input type="radio" name="name">Male
+<input type="radio" name="name">Femal<br><br>
+what language do you know<br><br>
+<input type="checkbox" name="en">English
+<input type="checkbox" name="hi">Hindi
+<input type="checkbox" name="fran">France
+<input type="checkbox" name="brit">British<br></br>
+<textarea cols=40 rows="10"></textarea>
+<input type="submit" value="submit" name="sub">
+
+
+</form>
+</body>
+</html>
